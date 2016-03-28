181d5802ab3005b07482c11305072444b9525581
Kitravee <Kitravee@Kitravee-PC>
2016-03-28 14:11:48 +0700

    new commit

--- src/indexing_project/Indexing_project.java
+++ src/indexing_project/Indexing_project.java
@@ -15,7 +15,7 @@ public class Indexing_project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("555");
+        System.out.println("kitravee");
     }
     
 }
